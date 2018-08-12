a2e288e16d9a46cc975688d7807996c74fd04220
Mohamed Nazim <egynazim@gmail.com>
2018-08-12 19:57:34 -0400

    added api data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,40 @@ import Title from './components/Title';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+
+const API_KEY = '0ae7b4af59511c3f6acd58a608fd45ac';
+
+
 class App extends React.Component{
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        console.log(data);
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+        });
+    }
     render(){
         return (
             <div>
                 <Title />
-                <Form />
+                <Form getWeather={this.getWeather}/>
                 <Weather />
             </div>
         );
