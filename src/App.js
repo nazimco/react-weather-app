57de6e93451d4fbe9517ee5585590839c70727f6
Mohamed Nazim <egynazim@gmail.com>
2018-08-17 16:31:37 -0400

    added image

--- src/App.js
+++ src/App.js
@@ -7,70 +7,69 @@ import Weather from './components/Weather';
 const API_KEY = '0ae7b4af59511c3f6acd58a608fd45ac';
 
 
-class App extends React.Component{
+class App extends React.Component {
     state = {
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: undefined
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
     }
     getWeather = async (e) => {
-        e.preventDefault();
-        const city = e.target.elements.city.value;
-        const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
-        const data = await api_call.json();
-       if (city && country) {
+      e.preventDefault();
+      const city = e.target.elements.city.value;
+      const country = e.target.elements.country.value;
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+      const data = await api_call.json();
+      if (city && country) {
         this.setState({
-            temperature: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-            error: ""
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
         });
-       } else {
+      } else {
         this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please enter the value."
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Please enter the values."
         });
-       }
-
+      }
     }
-    render(){
-        return (
-            <div>
-        <div className="wrapper">
-          <div className="main">
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-5 title-container">
-                  <Title />
-                </div>
-                <div className="col-xs-7 form-container">
-                  <Form getWeather={this.getWeather} />
-                  <Weather 
-                    temperature={this.state.temperature} 
-                    humidity={this.state.humidity}
-                    city={this.state.city}
-                    country={this.state.country}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
+    render() {
+      return (
+        <div>
+          <div className="wrapper">
+            <div className="main">
+              <div className="container">
+                <div className="row">
+                  <div className="col-xs-5 title-container">
+                    <Title />
+                  </div>
+                  <div className="col-xs-7 form-container">
+                    <Form getWeather={this.getWeather} />
+                    <Weather 
+                      temperature={this.state.temperature} 
+                      humidity={this.state.humidity}
+                      city={this.state.city}
+                      country={this.state.country}
+                      description={this.state.description}
+                      error={this.state.error}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-};
-
-export default App;
+      );
+    }
+  };
+  
+  export default App;
