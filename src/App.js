79cd21398346cefccfb764e14410c8f7d1f29e12
Mohamed Nazim <egynazim@gmail.com>
2018-08-17 16:17:42 -0400

    adjusted home picture

--- src/App.js
+++ src/App.js
@@ -46,20 +46,31 @@ class App extends React.Component{
     render(){
         return (
             <div>
-                <Title />
-                <Form getWeather={this.getWeather}/>
-                <Weather 
-                    temperature={this.state.temperature}
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature} 
+                    humidity={this.state.humidity}
                     city={this.state.city}
                     country={this.state.country}
-                    humidity={this.state.humidity}
                     description={this.state.description}
                     error={this.state.error}
-                />
+                  />
+                </div>
+              </div>
             </div>
-        );
-    }
-
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default App;
